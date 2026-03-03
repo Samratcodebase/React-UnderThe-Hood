e52e88d1cb1419ba6b0b8768859888dd71623171
Samratcodebase <samratcodebase@gmail.com>
2026-03-03 18:40:07 +0530

    Comment  v1 Code

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const RenderComponent = (reactElement, root) => {
+// const RenderComponent = (reactElement, root) => {
+//   const domElement = document.createElement(reactElement.type);
+//   domElement.innerHTML = reactElement.Children;
+//   domElement.setAttribute("href", reactElement.props.href);
+//   domElement.setAttribute("target", reactElement.props.target);
+//   root.appendChild(domElement);
+// };
+
+const RenderComponentV2 = (reactElement, root) => {
   const domElement = document.createElement(reactElement.type);
   domElement.innerHTML = reactElement.Children;
-  domElement.setAttribute("href", reactElement.props.href);
-  domElement.setAttribute("target", reactElement.props.target);
+  for (prop in reactElement.props) {
+    if (prop == "children") continue;
+
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+
   root.appendChild(domElement);
 };
 const reactElement = {
@@ -10,9 +22,13 @@ const reactElement = {
   props: {
     href: "https://google.com",
     target: "_blank",
+    id: "Samrat",
+    draggable: "true",
   },
   Children: "click  me to visit google",
 };
 
 const root = document.getElementById("root");
-RenderComponent(reactElement, root);
+RenderComponentV2(reactElement, root);
+
+
